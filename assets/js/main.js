250d2b9c3034edebb5c0e9ad6482aeec8ed1ab1f
Victor Asuquo Effiong <vaedesigner@gmail.com>
2024-08-12 16:12:28 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText", {
-  strings: ["Developer", "UX Designer"],
+  strings: ["Website Developer", "UX Designer"],
   loop: true,
   typeSpeed: 100,
   backSpeed: 80,
